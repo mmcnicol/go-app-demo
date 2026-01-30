afae6bd75487963b9b50440cc2c6aac40e2ec70d
M McNicol <matthew.mcnicol@gmail.com>
2026-01-30 00:17:25 +0000

    WIP

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -8,9 +8,9 @@ import (
 
 func main() {
 	// Register the components that correspond to routes
-	app.Route("/", func() app.UI { return &pages.LoginPage{} } )
-	app.Route("/home", func() app.UI { return &pages.HomePage{} } )
-	app.Route("/dev/storybook", func() app.UI { return &pages.ComponentGallery{} } )
+	app.Route("/", &pages.LoginPage{})
+	app.Route("/home", &pages.HomePage{})
+	app.Route("/dev/storybook", &pages.ComponentGallery{})
 
 	// This function starts the Wasm app in the browser.
 	// It stays idle when running on the server.
